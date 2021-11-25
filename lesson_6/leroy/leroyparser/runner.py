bf3c7bac56c4960164f9e01fc1babc31f9dffb6c
stanislaw8 <pavlovinthesky@gmail.com>
2021-11-25 11:02:40 +0300

    fixes

--- lesson_6/leroy/leroyparser/runner.py
+++ lesson_6/leroy/leroyparser/runner.py
@@ -1,12 +1,24 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-
+import sys
 from leroyparser.spiders.leroy import LeroySpider
 from leroyparser import settings
 
 if __name__ == '__main__':
+
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(LeroySpider)
+    try:
+        pages = int(sys.argv[1])
+        process.crawl(LeroySpider, pages=pages)
+
+    except IndexError:
+        process.crawl(LeroySpider)
+
+    except ValueError as e:
+        print(e)
+        print('Аргумент должен быть int')
+        sys.exit(1)
+
     process.start()
--- lesson_6/leroy/leroyparser/spiders/leroy.py
+++ lesson_6/leroy/leroyparser/spiders/leroy.py
@@ -1,24 +1,50 @@
-import scrapy
-from scrapy.http import HtmlResponse
 import logging
+
+import scrapy
 from leroyparser.items import LeroyparserItem
+from scrapy.http import HtmlResponse
 
 
 class LeroySpider(scrapy.Spider):
     name = 'leroy'
     allowed_domains = ['leroymerlin.ru']
+    current_page = 1
+    start_urls = [f'https://leroymerlin.ru/catalogue/elektroinstrumenty/']
 
-    def __init__(self, **kwargs):
-        self.start_urls = [f'https://leroymerlin.ru/catalogue/elektroinstrumenty']
+    def __init__(self, pages=1, **kwargs):
+        super().__init__(**kwargs)
+        self.pages = pages
 
     def parse(self, response: HtmlResponse):
-        ads_links = response.xpath('//a[contains(@data-qa, "product-image")]/@href').extract()
-        for link in ads_links:
-            yield response.follow('https://leroymerlin.ru' + link, callback=self.parseimg)
+        print(f'Парcинг {self.current_page}-й страницы...')
+        products_links = response.xpath(
+            '//a[contains(@data-qa, "product-image")]/@href').extract()
 
-    def parseimg(self, response: HtmlResponse):
+        for link in products_links:
+
+            yield response.follow('https://leroymerlin.ru' + link,
+                                  callback=self.parse_img)
+
+        if self.current_page < self.pages:
+            try:
+                next_page = 'https://leroymerlin.ru' + response.xpath(
+                    '//a[contains(@data-qa-pagination-item, "right")]/@href'
+                ).extract()[0]
+                self.current_page += 1
+
+                yield scrapy.Request(response.urljoin(next_page),
+                                     callback=self.parse)
+            except IndexError:
+                print('Страницы закончились')
+
+    def parse_img(self, response: HtmlResponse):
         name = response.xpath('//h1//text()').extract()[0]
-        price = response.xpath('//div[@class="product-detailed-page"]/@data-product-price').extract()[0]
-        photos = response.xpath('//img[contains(@alt, "product image")]/@src').extract()
+        price = response.xpath(
+            '//div[@class="product-detailed-page"]/@data-product-price'
+        ).extract()[0]
+        link = response.url
+        photos = response.xpath(
+            '//img[contains(@alt, "product image")]/@src').extract()
 
-        yield LeroyparserItem(name=name, photos=photos, price=price)
+        yield LeroyparserItem(name=name, price=price,
+                              link=link, photos=photos)
